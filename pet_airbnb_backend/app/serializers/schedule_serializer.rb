d919ebe2130bb552c0be22e84ee2dbdb97d0ce78
dizhengcindy <dizhengcindy@gmail.com>
2020-09-05 16:54:58 -0700

    modify serializer

--- pet_airbnb_backend/app/serializers/schedule_serializer.rb
+++ pet_airbnb_backend/app/serializers/schedule_serializer.rb
@@ -1,4 +1,6 @@
 class ScheduleSerializer < ActiveModel::Serializer
+  include Rails.application.routes.url_helpers
+  
   attributes :id,:num_of_pets,:start_date,:start_time,:end_date,
   :end_time, :rating, :comment, :done,:totalCost,:companyservice, :user, pictures:[]
 end
