69350a896750fa51563728e354d7e8623c8a871b
dizhengcindy <dizhengcindy@gmail.com>
2020-09-05 17:06:34 -0700

    modify serializer

--- pet_airbnb_backend/app/serializers/schedule_serializer.rb
+++ pet_airbnb_backend/app/serializers/schedule_serializer.rb
@@ -1,6 +1,20 @@
 class ScheduleSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
-  
+
   attributes :id,:num_of_pets,:start_date,:start_time,:end_date,
-  :end_time, :rating, :comment, :done,:totalCost,:companyservice, :user, pictures:[]
+  :end_time, :rating, :comment, :done,:totalCost,:companyservice, :user, :picture
+
+
+  def picture
+    return unless object.picture.attached?
+  
+    object.picture.blob.attributes
+          .slice('filename', 'byte_size')
+          .merge(url: picture_url)
+          .tap { |attrs| attrs['name'] = attrs.delete('filename') }
+  end
+  
+  def picture_url
+    url_for(object.picture)
+  end 
 end
