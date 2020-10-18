5b13a962aa9f0b6bcc3e430b4490df647e0d15df
dizhengcindy <dizhengcindy@gmail.com>
2020-10-18 14:40:59 -0500

    message

--- pet_airbnb_backend/app/serializers/schedule_serializer.rb
+++ pet_airbnb_backend/app/serializers/schedule_serializer.rb
@@ -2,7 +2,7 @@ class ScheduleSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
   attributes :id,:num_of_pets,:start_date,:start_time,:end_date,
-  :end_time, :rating, :comment, :done,:totalCost,:companyservice, :user, :picture
+  :end_time, :rating, :comment, :done,:totalCost,:companyservice, :user, pictures:[]
 
 
   def picture
